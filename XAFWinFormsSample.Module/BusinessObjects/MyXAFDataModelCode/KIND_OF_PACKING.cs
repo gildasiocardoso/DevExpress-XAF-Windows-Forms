84a8e01bd67164238ac4c19d2888349c9f145181
Gildasio Cardoso <31892273+gildasiocardoso@users.noreply.github.com>
2018-11-25 22:13:51 -0500

    initial commit
    initial commit

---
+++ XAFWinFormsSample.Module/BusinessObjects/MyXAFDataModelCode/KIND_OF_PACKING.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+
+namespace XAFWinFormsSample.Module.BusinessObjects.Database
+{
+    [DefaultClassOptions, ImageName("BO_KIND_OF_PACKING")]
+    public partial class KIND_OF_PACKING
+    {
+        public KIND_OF_PACKING(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
